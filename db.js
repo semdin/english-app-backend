b92d9a7dfda19e77770d06d6fa04f0a637af9733
semdin <mehmetsemdin.aktay@std.yeditepe.edu.tr>
2024-09-26 22:02:45 +0300

    db change according to vercel

--- db.js
+++ db.js
@@ -1,13 +1,11 @@
-// db.js
 const { Pool } = require("pg");
 require("dotenv").config();
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
+  connectionString: process.env.POSTGRES_URL, // Use the Vercel-provided URL
+  ssl: {
+    rejectUnauthorized: false, // Important for using SSL connections (Vercel usually requires SSL)
+  },
 });
 
 module.exports = pool;
